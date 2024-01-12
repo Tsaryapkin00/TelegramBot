c1a7fbf949891b11a521616ca6f790b099cdee61
Tsaryapkin-Konstantin <117481592+Tsaryapkin00@users.noreply.github.com>
2024-01-12 18:04:28 +0300

    Update app.py

--- Проектный бот/app.py
+++ Проектный бот/app.py
@@ -6,17 +6,45 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def helping(message: telebot.types.Message):
-    text = 'Для начала работы введите команду боту в формате: \n <название валюты> \
-<название валюты в которую надо перевести>\
-<количество переводимой валюты>\nЧтобы увидеть список доступных валю введите: /values'
+    text = texta
     bot.reply_to(message, text)
+
+@bot.message_handler(commands=['description',])
+def description(message: telebot.types.Message):
+    txt = txt1
+    bot.reply_to(message, txt)
 
 @bot.message_handler(commands=['values', ])
 def values(message: telebot.types.Message):
     text = 'Доступны валюты:'
     for key in keys.keys():
-        text = '\n'.join((text, key, ))
+        text = '\n           '.join((text, key, ))
     bot.reply_to(message, text)
+
+@bot.message_handler(commands=['dollar', ])
+def dollar(message: telebot.types.Message):
+    val = 'доллар рубль 1'
+    value = val.split(' ')
+
+    quote, base, amount = value
+
+    total_base = ValuteConvert.get_price(quote, base, amount)
+
+    text = f'Курс доллара к рублю сейчас -   {total_base}'
+    bot.send_message(message.chat.id, text)
+
+
+@bot.message_handler(commands=['euro', ])
+def euro(message: telebot.types.Message):
+    val = 'евро рубль 1'
+    value = val.split(' ')
+
+    quote, base, amount = value
+
+    total_base = ValuteConvert.get_price(quote, base, amount)
+
+    text = f'Курс евро к рублю сейчас -   {total_base}'
+    bot.send_message(message.chat.id, text)
 
 @bot.message_handler(content_types=['text', ])
 def convert(message: telebot.types.Message):
@@ -33,7 +61,7 @@ def convert(message: telebot.types.Message):
     except Exception as e:
         bot.reply_to(message, f'Не удалось обработать команду\n{e}')
     else:
-        text = f'Цена {amount} {quote} в {base} - {total_base}'
+        text = f'Цена {amount} {quote} в {base} -   {total_base}'
         bot.send_message(message.chat.id, text)
 
 
